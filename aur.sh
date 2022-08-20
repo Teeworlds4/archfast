032fad10ee37c73d0f652ca61388956df004a557
Adam <42157493+Teeworlds4@users.noreply.github.com>
2022-08-20 19:21:36 +0200

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -2,8 +2,12 @@ mkdir tmp
 cd tmp
 git clone https://aur.archlinux.org/yay.git
 git clone https://aur.archlinux.org/pamac-aur.git
-cd yay && makepkg -si && cd ..
-cd pamac-aur && makepkg -si && cd ..
+cd yay 
+makepkg -si 
+cd ..
+cd pamac-aur
+makepkg -si
+cd ..
 cd ..
 rm tmp -rf
 reboot
