b0a6901a7dceda4409f2285c446de8c236ae3cb5
Adam <42157493+Teeworlds4@users.noreply.github.com>
2022-08-20 17:05:09 +0200

    Add files via upload

---
+++ aur.sh
@@ -0,0 +1,7 @@
+mkdir tmp && cd tmp
+git clone https://aur.archlinux.org/yay.git
+git clone https://aur.archlinux.org/pamac-aur.git
+cd yay && makepkg -si && cd ..
+cd pamac-aur && makepkg -si && cd ..
+cd ..
+rm tmp -rf
